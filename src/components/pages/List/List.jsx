abffabb4e9923ee02f494c680459dc02e42ad450
xing <1229067610@qq.com>
2018-12-03 21:30:49 +0800

    List

--- src/components/pages/List/List.jsx
+++ src/components/pages/List/List.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../../styles/product.scss';
+import '../../../styles/list.scss';
 import { Link } from 'react-router-dom';
 class List extends React.Component{
     constructor(props) {
@@ -7,10 +7,16 @@ class List extends React.Component{
         this.props = props;
         this.state = {
             list:[],
-            leiMing:false
+            List:[],
+            qieHuan:[
+            	{name:'新品',sort:2},
+            	{name:'畅销',sort:1},
+            	{name:'价格',sort:3}
+            ]
         }
     }
     componentDidMount(){
+
     	//获取传过来的id
     	let itemindexid = this.props.match.params.ItemIndexId;
     	let filter = this.props.match.params.filter;
@@ -18,7 +24,8 @@ class List extends React.Component{
         React.axios.get('http://app.lifevc.com/1.0/v_h5_5.1.2_33/Categories/Category?itemindexid='+itemindexid+'&filter='+filter+'&sort=0&o=http%3A%2F%2Fm.lifevc.com&NewCartVersion=true')
         .then((res)=>{
 	        this.setState({
-	            list: res.data.InnerData
+	            list: res.data.InnerData.GoodsItems,
+	            List:res.data.InnerData
 	        });
             console.log(this.state.list,itemindexid,filter)
             
@@ -31,11 +38,50 @@ class List extends React.Component{
     
     render(){
         return (
-           <div>111</div>
+           <div>
+           		<div className="Topp1">
+           			<div className="TLeft">
+           				<Link to="/footer/all/" >
+           					<i className="fa fa-chevron-left" aria-hidden="true"></i>
+           				</Link>
+           			</div>
+           			<div className="TRight">
+           				{this.state.List.ItemIndexName}
+           			</div>
+           		</div>
+
+           		<div>
+           			<ul className="qieHuan">
+           			{(()=>{
+           				return this.state.qieHuan.map((item,index)=>{
+           					return (
+           						<li key={index}><a href="#">{item.name}</a></li>
+           					)
+           				})
+           			})()}
+           			</ul>
+           		</div>
+
+           		<div className="XunH">
+           			{(()=>{
+           				return this.state.list.map((item,index)=>{
+           					return (
+           						<div key={index} className="danDu">
+           							<div className="Zp"><img src={"http://i.lifevccdn.com"+item.ImageUrl} alt={item.Name}/></div>
+	           						<div className="PPP"><p>{item.Appeal}</p></div>
+	           						<div className="PriCt">
+	           							<div className="PriCt-Left">￥<span>{item.SalePrice}</span></div>
+	           							<div className="PriCt-Right">评论：<span>{item.CommentCount}</span></div>
+	           						</div>
+           						</div>
+           						
+           					)
+           				})
+           			})()}
+           		</div>
+           		
+           </div>
         )
     }
 }
 export default List;
-// http://app.lifevc.com/1.0/v_h5_5.1.2_33/Categories/Category?itemindexid=2861&filter=3552&sort=0&o=http%3A%2F%2Fm.lifevc.com&NewCartVersion=true
-// http://app.lifevc.com/1.0/v_h5_5.1.2_33/Categories/Category?itemindexid=2861&filter=3458&sort=0&o=http%3A%2F%2Fm.lifevc.com&NewCartVersion=true
-// http://app.lifevc.com/1.0/v_h5_5.1.2_33/Categories/Category?itemindexid=3526&filter=3530&sort=0&o=http%3A%2F%2Fm.lifevc.com&NewCartVersion=true
