b5e2ee1f906b757d96952886a092207edd59ae6e
pangziguai <m13413684903@163.com>
2018-12-05 12:59:05 +0800

    增加了解页

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import Footer from './components/commons/footer/Footer.jsx';
 import List from './components/pages/List/List.jsx';
 import Detail from './components/pages/Detail/Detail.jsx';
+import LearnMore from './components/pages/Home/LearnMore';
 import './styles/reset.scss';
+// import '../node_modules/animate.css/animate.css';
 import { Route,Redirect,Switch} from "react-router-dom";
 class App extends Component {
   render() {
@@ -14,6 +16,7 @@ class App extends Component {
             <Route path="/detail/:ItemInfoId" component={Detail} />
             <Redirect from='/' to='/footer/home/index/'/>
           </Switch>
+          <LearnMore></LearnMore>
       </div> 
     )
   }
